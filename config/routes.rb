6a39d49e3d01302dde027251d53bfb941c43339d
Frank Gerngross <f.gerngross@googlemail.com>
2022-08-11 21:44:13 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user_collections, only: :destroy
+  resources :boxes, except: [:new, :create] do
+    resources :items, only: [:new, :create]
+  end
+  resources :items, except: [:new, :create]
+  resources :collections do
+    resources :user_collections, only: [:new, :create]
+    resources :boxes, only: [:new, :create]
+  end
 end
