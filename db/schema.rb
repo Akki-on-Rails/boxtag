d9c81b666be5eecd85e300864bf1c7295977d827
Jordan Patel <logicaljordan247@gmail.com>
2022-08-13 14:09:05 +0200

    changed links

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_184422) do
+ActiveRecord::Schema.define(version: 2022_08_13_090319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boxes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "collection_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["collection_id"], name: "index_boxes_on_collection_id"
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_collection_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_collection_id"], name: "index_collections_on_user_collection_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.bigint "box_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["box_id"], name: "index_items_on_box_id"
+  end
+
+  create_table "user_collections", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_184422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boxes", "collections"
+  add_foreign_key "collections", "user_collections"
+  add_foreign_key "items", "boxes"
 end
