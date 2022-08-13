7cbdd6c3031ce8a5dea49d3499dd83ec306764a2
Frank Gerngross <f.gerngross@googlemail.com>
2022-08-13 13:16:25 +0200

    created validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :user_collection
+  # belongs_to :user_collection
+  # to sign up as a new user we need a default collection that
+  # is linked when creating the new user.
+  has_one_attached :avatar
 end
