2f0a7a021c879bf95f402580445f2bc059d3af7d
Frank Gerngross <f.gerngross@googlemail.com>
2022-08-16 21:11:05 +0200

    added PgSearch (multisearchable)

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,21 @@ class Collection < ApplicationRecord
   validates :description, presence: true, length: { minimum: 8 }
   has_many :user_collections
   has_many :boxes
+
+  include PgSearch::Model
+  multisearchable against: [:name]
+  PgSearch.multisearch_options = {
+    using: {:tsearch => { :prefix => true }}
+  }
+
+  # include PgSearch::Model
+  # pg_search_scope :global_search,
+  #   against: [ :name ],
+  #   associated_against: {
+  #     box: [ :name ],
+  #     item: [ :name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 end
