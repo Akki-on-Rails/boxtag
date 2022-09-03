acf97a05141c30a6725b583d1ec880b4556c1719
philipp kern <philippk8@gmail.com>
2022-09-03 10:13:21 +0200

    beep

--- app/javascript/packs/basic-qr-code-reader.js
+++ app/javascript/packs/basic-qr-code-reader.js
@@ -4,13 +4,17 @@ const codeReader = new BrowserQRCodeReader();
 
 codeReader
   .decodeFromInputVideoDevice(undefined, 'video')
-  .then((result) => {
+  .then(() => {
     // process the result
-    console.log(result.text)
 
+    document.getElementById('beep-audio').play()
+
+
+
+  }).then((result) => {
+    console.log(result.text)
     document.getElementById('result').textContent = result.text
     window.location.replace(result.text)
-    // document.getElementById('beep-audio').play()
 
   })
   .catch(err => console.error(err));
