92fe536d42461bac972dc380f1cc7b81a2df95d4
Jordan Patel <logicaljordan247@gmail.com>
2022-09-03 10:56:43 +0200

    added icon target to controller

--- app/javascript/controllers/edit_resource_controller.js
+++ app/javascript/controllers/edit_resource_controller.js
@@ -1,14 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "infos", "form" ]
+  static targets = [ "infos", "form", "icons" ]
 
   connect() {
-    console.log("connected")
+    this.myNumber = 1
+    console.log(this)
   }
-  displayForm() {
-    this.infosTarget.classList.add('d-none');
-    this.formTarget.classList.remove('d-none');
+  displayForm(e) {
+    // this.infosTarget.classList.add('d-none');
+    // this.formTarget.classList.remove('d-none');
+    const index = e.currentTarget.dataset.index
+    console.log(this.infosTargets[0])
+    console.log(this.iconsTargets)
+    console.log(index,typeof index)
+    if(!index) {
+      this.infosTargets[0].classList.add('d-none');
+      this.formTargets[0].classList.remove('d-none');
+    } else {
+      this.infosTargets[Number(index) + 1].classList.add('d-none');
+      this.formTargets[Number(index) + 1].classList.remove('d-none');
+      this.iconsTargets[Number(index)].classList.add('d-none');
+    }
   }
 
   update() {
